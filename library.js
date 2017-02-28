3c694253c57f2ed20d78d5dc7b9ad42f85d02082
Bill McDowell <forkunited@gmail.com>
2017-02-28 13:30:16 -0800

    ...

--- library.js
+++ library.js
@@ -1,11 +1,11 @@
-var util = require("./util");
+/*var util = require("./util");
 var counter = require("./counter");
 var bilookup = require("./bilookup")
 var matrix = require("./matrix");
 var rgame = require("./rgame");
 var feature = require("./feature");
 var evaluation = require("./evaluation.js");
-
+*/
 /*var dist = require("./dist");
 var evaluation = require("./evaluation");
 var semantics = require("./semantics");
@@ -14,13 +14,13 @@ var logic = require("./logic");
 var util = require("./util");*/
 
 module.exports = {
-	util : util,
+	/*util : util,
 	counter : counter,
 	bilookup : bilookup,
     matrix : matrix,
 	rgame : rgame,
 	feature : feature,
-	evaluation : evaluation/*,
+	evaluation : evaluation*//*,
 	dist : dist,
 	evaluation : evaluation,
 	space : space,
